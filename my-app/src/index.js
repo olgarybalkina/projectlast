9a6145048bc9aca611c35af45a1fddf5cc066ee1
Aleksandr Anisenko <aleksandr.anisenko@mail.ru>
2020-12-06 13:58:49 +0300

    Added Animation

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import mainLayout from './commonComponents/layouts/mainLayout.js'
+ReactDOM.render(
+  <React.StrictMode>
+    <mainLayout />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
